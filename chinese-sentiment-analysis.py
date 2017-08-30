0d910f62800b418e9fb836bd10aa8d2a1c0904cf
Zbored <594457215@qq.com>
2017-08-30 15:26:36 +0800

    20170830

--- chinese-sentiment-analysis.py
+++ chinese-sentiment-analysis.py
@@ -17,18 +17,31 @@ def load_file_and_preprocessing():
     pos=pd.read_excel('data/pos.xls',header=None,index=None)
 
     cw = lambda x: list(jieba.cut(x))
+
+    # 新增一列 word ,存放分好词的评论，pos[0]代表表格第一列
+
     pos['words'] = pos[0].apply(cw)
     neg['words'] = neg[0].apply(cw)
 
-    #print pos['words']
-    #use 1 for positive sentiment, 0 for negative
-    y = np.concatenate((np.ones(len(pos)), np.zeros(len(neg))))
+    # np.ones(len(pos)) 新建一个长度为len(pos)的数组并初始化元素全为1来标注好评
+    # np.concatenate（）连接数组
+    # axis=0 向下执行方法 axis=1向右执行方法
+    y = np.concatenate((np.ones(len(pos)), np.zeros(len(neg))),axis=0)
+
+    # train_test_split：从样本中随机的按比例选取train data和testdata
+    # 一般形式：train_test_split(train_data,train_target,test_size=0.4, random_state=0)
+    # train_data：所要划分的样本特征集
+    # train_target：所要划分的样本结果（标注）
+    # test_size：样本占比，如果是整数的话就是样本的数量
+    # random_state：是随机数的种子。
 
     x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos['words'], neg['words'])), y, test_size=0.2)
     
     np.save('svm_data/y_train.npy',y_train)
     np.save('svm_data/y_test.npy',y_test)
     return x_train,x_test
+
+
 
 # 对每个句子的所有词向量取均值，来生成一个句子的vector
 def build_sentence_vector(text, size,imdb_w2v):
@@ -45,28 +58,26 @@ def build_sentence_vector(text, size,imdb_w2v):
     return vec
 
 # 计算词向量
-def get_train_vecs(x_train,x_test):
+def get_train_vecs(x_train, x_test):
     n_dim = 300
-    #初始化模型和词表
-    imdb_w2v = Word2Vec(x_train,size=n_dim, min_count=10)
-    #imdb_w2v.build_vocab(x_train)
-    
-    #在评论训练集上建模
-    #imdb_w2v.train(x_train)
-    
-    train_vecs = np.concatenate([build_sentence_vector(z, n_dim,imdb_w2v) for z in x_train])
-    #train_vecs = scale(train_vecs)
-    
-    np.save('svm_data/train_vecs.npy',train_vecs)
+    # 初始化模型和词表
+    imdb_w2v = Word2Vec(x_train, size=n_dim, min_count=10)
+
+    train_vecs = np.concatenate([build_sentence_vector(z, n_dim, imdb_w2v) for z in x_train])
+    # train_vecs = scale(train_vecs)
+
+    np.save('svm_data/train_vecs.npy', train_vecs)
     print train_vecs.shape
-    #在测试集上训练
+    # 在测试集上训练
     imdb_w2v.train(x_test)
     imdb_w2v.save('svm_data/w2v_model/w2v_model.pkl')
-    #Build test tweet vectors then scale
-    test_vecs = np.concatenate([build_sentence_vector(z, n_dim,imdb_w2v) for z in x_test])
-    #test_vecs = scale(test_vecs)
-    np.save('svm_data/test_vecs.npy',test_vecs)
+    # Build test tweet vectors then scale
+    test_vecs = np.concatenate([build_sentence_vector(z, n_dim, imdb_w2v) for z in x_test])
+    # test_vecs = scale(test_vecs)
+    np.save('svm_data/test_vecs.npy', test_vecs)
     print test_vecs.shape
+
+
 
 def get_data():
     train_vecs=np.load('svm_data/train_vecs.npy')
